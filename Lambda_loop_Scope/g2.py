745d69c29231a055dd926a7989d2ed166cccaae3
sarisma123 <sarishmaghimire45@gmail.com>
2026-08-12 10:38:05 +0545

    few changes

--- Lambda_loop_Scope/g2.py
+++ Lambda_loop_Scope/g2.py
@@ -4,3 +4,22 @@ def func():
     print("inside function",x)
 func()
 print("outside the function",x)
+
+x=100
+def func():
+    global x
+    x=55
+    print("inside function",x)
+print("outside function before editing",x)
+func()
+print("outside function after editing",x)
+
+# task
+x=20
+def process():
+    x=20
+    print(x)
+process()
+
+# wap showing the manupulation of global variable
+
